503664584599d3a2534af8fdc745b9cee5f431e5
Jessica Daley <79805880+JessicaLDaley@users.noreply.github.com>
2021-09-18 12:43:21 -0400

    Add files via upload

---
+++ README-Generator/Develop/index.js
@@ -0,0 +1,22 @@
+const inquirer = require('inquirer');// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+// const questions = [];
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
